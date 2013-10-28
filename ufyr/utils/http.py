b3676782d78296c3ed49bd7bc269b7e28bcd941c
Adi Foulger <adi.foulger@zefr.com>
2013-10-28 12:03:49 -0700

    Add Callback object

---
+++ ufyr/utils/http.py
@@ -0,0 +1,63 @@
+#! /usr/bin/python
+
+import json
+
+import requests
+
+class Callback(object):
+    def __init__(self, url, method='GET', req_kwargs={}, **kwargs):
+        
+        assert isinstance(url, (str, unicode))
+        assert isinstance(method, (str, unicode))
+        assert isinstance(req_kwargs, dict)
+        
+        
+        self.url = url
+        self.method = method
+        self.req_kwargs = req_kwargs
+    
+    @classmethod
+    def from_json(cls, json_string):
+        '''
+        Input:  string json_string containing a url key
+        
+        Returns: a Callback obj instance initalized by the parameters in the json_string.
+        '''
+        json_obj = json.loads(json_string)
+        
+        if 'url' not in json_obj:
+            raise Exception('"url" not in json')
+        
+        return cls.__init__(**json_obj)
+    
+    def to_json(self):
+        '''
+        Return a JSON serialization of the Callback obj.
+        '''
+        json_obj = {'url':self.url,
+                    'method':self.method,
+                    'req_kwargs':self.req_kwargs}
+        
+        return json.dumps(json_obj)
+    
+    def execute(self):
+        f = getattr(requests, self.method.lower())
+        f(self.url, **self.req_kwargs)
+        
+        
+    
+        
+        
+        
+    
+
+
+
+def process_callback(callback_obj):
+    '''
+    Given a string or dict with callback info process that callback.
+    '''
+    data = {'callback':{'method':'POST', 'url':'http://localhost', 'kwargs':{'data':{'id':'foo'}}}}
+    
+    
+    
