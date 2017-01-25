7337ea62eff69fcc3708e447ca8ede95a80b79df
Adi Foulger <technobabelfish@gmail.com>
2017-01-24 21:11:19 -0800

    Remove oooooollld random script and add some working copy stuff, no idea what I was doing but hey

--- ufyr/functions.py
+++ ufyr/functions.py
@@ -1,16 +1,19 @@
-
+#stdlib
+import traceback
 from time import sleep
 
+#local
 from logger import Logger
 from utils.http import Callback
 
 
-def do(func_list, metadata_class, retry_limit=None, log_path='/var/log/do.log', callback=None, **kwargs):
+def do(func_list, metadata_class, retry_limit=None, log_name='/var/log/do.log', callback=None, **kwargs):
     '''
     Logging/retry logic wrapper.
     
     Inputs:
         func_list: list of tuples [(func, (args), {kwargs})] - args vs kwargs works on type identification
+        metadata_class - instance of MetadataBase - see metadata.py in same repo
         retry_limit: int, maximum retries for a given function - not to exceed settings.RETRY_THRESHOLD
         log_name: string name of log file that do should write to
         callback: string callback json from Callback.to_json to execute upon func_list completion
@@ -34,7 +37,7 @@ def do(func_list, metadata_class, retry_limit=None, log_path='/var/log/do.log', 
         fkwargs.update(kwargs)
         
         logger.info('%s\tBEGIN\targs: %s\tkwargs: %s', f_name, args, fkwargs)
-        retry = retry_limit if retry_limit and retry_limit <= RETRY_THRESHOLD else RETRY_THRESHOLD
+        retry = retry_limit if retry_limit else 1
         
         success = False
         for i in xrange(retry):
@@ -64,3 +67,56 @@ def do(func_list, metadata_class, retry_limit=None, log_path='/var/log/do.log', 
         logger.info('Callback complete')
         
     logger.info('DONE')
+    
+    
+def _get_args_kwargs(f_data):
+    '''
+    Given a tuple of a length of 1-3 return a tuple of func, args, kwargs
+    
+    Inputs:
+        f_data - tuple (function, (args) or {kwargs}, {kwargs} or (args)) -- id'd off of type
+        
+    Returns:
+        a tuple of func, args, kwargs
+    '''
+    func = f_data[0]
+    
+    if isinstance(func, (str, unicode)):
+        func = __get_function(func)
+        
+    assert callable(func)
+    
+    arg_chunk = f_data[1:]
+    args = ()
+    kwargs = {}
+    
+    if len(arg_chunk) == 0:
+        pass
+    elif len(arg_chunk) == 1:
+        tmp = arg_chunk[0]
+        if isinstance(tmp, (list, tuple)):
+            args = tmp
+        elif isinstance(tmp, dict):
+            kwargs.update(tmp)
+        else:
+            raise Exception('args mapping failure: %s'%str(arg_chunk))
+    elif len(arg_chunk) == 2:
+        for args_item in arg_chunk:
+            if isinstance(args_item, (list, tuple)) and not args:
+                args = args_item
+            elif isinstance(args_item, dict):
+                kwargs.update(args_item)
+            else:
+                raise Exception('args mapping failure: %s'%str(arg_chunk))
+            
+    else:
+        raise Exception('args mapping failure: %s'%str(arg_chunk))
+    
+    return func, args, kwargs
+
+def __get_function(name):
+    """Return an attribute from a dotted path name (e.g. "path.to.func")."""
+    module_name, obj, method = name.rsplit('.', 2)
+    module = import_module(module_name)
+    var = getattr(module, obj)
+    return getattr(var, method)
